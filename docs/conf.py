fa7ef1f887e8b59ce8429edc6d688ae76fb66c1d
Jort Stammen <168363690+JortStamme@users.noreply.github.com>
2025-08-16 17:51:30 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,20 @@
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "MyManimLibrary"
+author = "Your Name"
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",      # API docs from docstrings
+    "sphinx.ext.napoleon",     # Support Google/NumPy-style docstrings
+    "sphinx.ext.viewcode",     # Link to source code
+    "sphinx_gallery.gen_gallery",  # For example gallery
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
