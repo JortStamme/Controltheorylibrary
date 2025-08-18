4463635c512baa9231c180ee5680bdfca6fc8308
Jort Stammen <168363690+JortStamme@users.noreply.github.com>
2025-08-18 21:35:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,31 @@
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath(".."))
 
-project = "MyManimLibrary"
-author = "Your Name"
+project = "Controltheorylib"
+author = "Jort Stammen"
 release = "0.1.0"
 
 extensions = [
-    "sphinx.ext.autodoc",      # API docs from docstrings
-    "sphinx.ext.napoleon",     # Support Google/NumPy-style docstrings
-    "sphinx.ext.viewcode",     # Link to source code
-    "sphinx_gallery.gen_gallery",  # For example gallery
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_gallery.gen_gallery",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
+
+# ------------------------
+# Sphinx-Gallery config
+# ------------------------
+from sphinx_gallery.sorting import ExampleTitleSortKey
+
+sphinx_gallery_conf = {
+    "examples_dirs": "Controltheorylib/examples",   # Folder with example .py files
+    "gallery_dirs": "auto_examples",  # Output folder for generated gallery
+    "within_subsection_order": ExampleTitleSortKey,
+    "remove_config_comments": True,
+}
